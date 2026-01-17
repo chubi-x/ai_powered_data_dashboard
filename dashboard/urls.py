cde8e4ee5da5ec3946d4e58591598d01a4be07ed
chubi-x <chubixax@protonmail.com>
2026-01-17 18:33:13 +0000

    feat: add api/charts endpoint for template-based chart rendering

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,11 +7,8 @@ app_name = "dashboard"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    # API endpoints
-    path("api/regions/", api_views.regions_list, name="api_regions"),
+    path("api/charts/", views.GetChartsView.as_view(), name="get_charts"),
     path("api/charts/timeseries", views.timeseries_chart, name="get_timeseries_chart"),
     path("api/charts/pie", views.pie_chart, name="get_pie_chart"),
-    # path("api/headline-stats/", api_views.headline_stats, name="api_headline_stats"),
-    path("api/modules/", api_views.modules_list, name="api_modules"),
     path("api/projections/", api_views.projections_list, name="api_projections"),
 ]
