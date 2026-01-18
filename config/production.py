abc1defd194d546dca1ed736960be885e4309230
chubi-x <chubixax@protonmail.com>
2026-01-18 07:47:06 +0000

    feat:productionise

--- config/production.py
+++ config/production.py
@@ -1,8 +1,19 @@
 import os
 from .base import *
 
-ALLOWED_HOSTS = ["https://dash.chubi.dev"]
+ALLOWED_HOSTS = ["dash.chubi.dev"]
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "dashboard"),
+        "USER": os.getenv("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
+        "HOST": "ai_dash_postgres",
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
+    }
+}
